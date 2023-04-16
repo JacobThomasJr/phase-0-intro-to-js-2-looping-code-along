a298d8a126c1e6327cb4c90dbc6152d921c3dade
jacobaranza <jacobthomasjunior@gmail.com>
2023-04-16 14:27:56 -0500

    looping code along

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(names, occasion) {
+    const thankYouMessages = [];
+    names.forEach(name => {
+      thankYouMessages.push(`Thank you, ${name}, for the wonderful ${occasion} gift!`);
+    });
+    return thankYouMessages;
+  }
+  
+  function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+      console.log(i);
+    }
+  }
+  
